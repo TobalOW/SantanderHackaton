5283a9e54c40bc0c8c38dcdd7b2f6844761978ab
TobalOW <cristobal.mirandau@mail.udp.cl>
2019-04-06 09:46:51 -0300

    bank screen parent done

--- src/screens/parent/BankScreen.js
+++ src/screens/parent/BankScreen.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React, {Component} from "react";
-import {View, Text, StyleSheet, Image, Dimensions} from "react-native";
+import {View, Text, StyleSheet, Image, Dimensions, ImageBackground} from "react-native";
 import {ListItem} from "react-native-elements";
 import {Constants} from "expo";
 import Feather from "react-native-vector-icons/Feather";
@@ -14,100 +14,102 @@ export default class BankScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Navbar
-          openDrawer={() => this.props.navigation.openDrawer()}
-          goTask={() => this.props.navigation.navigate("main")}
-        />
-        <View style={{flex: 1, backgroundColor: "#D7D6D6"}}>
-          <View style={{flex: 1}}>
-            <View
-              style={{
-                backgroundColor: "white",
-                margin: 10,
-                height: 45,
-                borderRadius: 45
-              }}
-            >
+        <ImageBackground source={require('../../assets/BG.jpg')} style={{ height: Dimensions.get('window').height }}>
+          <View style={{ flex: 1 }}>
+            <Navbar
+              openDrawer={() => this.props.navigation.openDrawer()}
+              goTask={() => this.props.navigation.navigate("main")}
+            />
+            <View style={{flex: 1}}>
               <View
                 style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  alignItems: "center"
+                  backgroundColor: "white",
+                  margin: 10,
+                  height: 45,
+                  borderRadius: 45
                 }}
               >
                 <View
                   style={{
-                    heigth: 45,
-                    padding: 13,
-                    alignItems: "center",
-                    justifyContent: "center"
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center"
                   }}
                 >
-                  <Text style={{fontSize: 17}}>Cristobal</Text>
-                </View>
-                <View
-                  style={{
-                    justifyContent: "flex-end",
-                    alignItems: "flex-end",
-                    paddingRight: 15
-                  }}
-                >
-                  <Feather
-                    name="chevron-down"
-                    color={"black"}
-                    style={{fontSize: 20}}
-                  />
+                  <View
+                    style={{
+                      heigth: 45,
+                      padding: 13,
+                      alignItems: "center",
+                      justifyContent: "center"
+                    }}
+                  >
+                    <Text style={{fontSize: 17}}>Cristobal</Text>
+                  </View>
+                  <View
+                    style={{
+                      justifyContent: "flex-end",
+                      alignItems: "flex-end",
+                      paddingRight: 15
+                    }}
+                  >
+                    <Feather
+                      name="chevron-down"
+                      color={"black"}
+                      style={{fontSize: 20}}
+                    />
+                  </View>
                 </View>
               </View>
-            </View>
-            <View
-              style={{
-                backgroundColor: "white",
-                marginLeft: 10,
-                marginRight: 10,
-                height: 45,
-                borderRadius: 45
-              }}
-            >
               <View
                 style={{
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  alignItems: "center"
+                  backgroundColor: "white",
+                  marginLeft: 10,
+                  marginRight: 10,
+                  height: 45,
+                  borderRadius: 45
                 }}
               >
                 <View
                   style={{
-                    heigth: 45,
-                    padding: 13,
-                    alignItems: "center",
-                    justifyContent: "center"
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignItems: "center"
                   }}
                 >
-                  <Text style={{fontSize: 17}}>
-                    Monto actual:
-                    <Text style={{fontWeight: "bold"}}> $5000</Text>
-                  </Text>
+                  <View
+                    style={{
+                      heigth: 45,
+                      padding: 13,
+                      alignItems: "center",
+                      justifyContent: "center"
+                    }}
+                  >
+                    <Text style={{fontSize: 17}}>
+                      Monto Guardado:
+                      <Text style={{fontWeight: "bold"}}> $8000</Text>
+                    </Text>
+                  </View>
                 </View>
               </View>
-            </View>
-            <View
-              style={{flex: 1, justifyContent: "center", alignItems: "center"}}
-            >
-              <Image
-                style={{
-                  width: SCREEN_WIDTH - 20,
-                  height: 400,
-                  resizeMode: "contain"
-                }}
-                source={require("../../assets/Calendario.png")}
-              />
-              <Text style={{fontSize: 17, color: "black"}}>
-                ¡Racha de <Text style={{fontWeight: "bold"}}>12 dias</Text>!
-              </Text>
+              <View
+                style={{flex: 1, alignItems: "center", paddingTop: 30}}
+              >
+                <Image
+                  style={{
+                    width: SCREEN_WIDTH - 20,
+                    height: 350,
+                    resizeMode: "contain"
+                  }}
+                  source={require("../../assets/Calendario.png")}
+                />
+              <Text style={{fontSize: 17, color: "white", marginTop: 13}}>
+                  ¡Racha de <Text style={{fontWeight: "bold"}}>12 días</Text>!
+                </Text>
+              </View>
             </View>
           </View>
-        </View>
+        </ImageBackground>
       </View>
     );
   }
